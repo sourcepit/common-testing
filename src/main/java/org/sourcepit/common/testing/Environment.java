1b5efbfed83f6d3a532085ac04d139fd4581a941
Bernd Vogt <bernd.vogt@sourcepit.org>
2012-02-28 08:35:26 +0100

    Added Environment + Tests

---
+++ src/main/java/org/sourcepit/common/testing/Environment.java
@@ -0,0 +1,248 @@
+/**
+ * Copyright (c) 2011 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.sourcepit.common.testing;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+/**
+ * @author Bernd
+ */
+public final class Environment
+{
+   private static final Map<String, Environment> environments = new HashMap<String, Environment>();
+
+   public static Environment getSystem()
+   {
+      return get(null);
+   }
+
+   public static Environment get(String path)
+   {
+      synchronized (environments)
+      {
+         Environment environment = environments.get(path);
+         if (environment == null)
+         {
+            environment = newEnvironment(path);
+            environments.put(path, environment);
+         }
+         return environment;
+      }
+   }
+
+   public static Environment newEnvironment(String path)
+   {
+      final Properties properties = new Properties();
+      properties.putAll(System.getProperties());
+      if (path != null)
+      {
+         properties.putAll(loadProperties(path));
+      }
+      return newEnvironment(new LinkedHashMap<String, String>(System.getenv()), properties);
+   }
+
+   public static Environment newEnvironment(final Map<String, String> envs, final Properties properties)
+   {
+      for (Entry<Object, Object> entry : properties.entrySet())
+      {
+         String key = entry.getKey().toString();
+         if (key.startsWith("env.") && key.length() > 4)
+         {
+            key = key.substring(4);
+            final Object value = entry.getValue();
+            envs.put(key, value == null ? null : value.toString());
+         }
+      }
+
+      return new Environment(envs, properties);
+   }
+
+   private final Map<String, String> envs;
+   private final Properties properties;
+
+   private static Properties loadProperties(String path)
+   {
+      Properties properties = new Properties();
+      ClassLoader cl = Environment.class.getClassLoader();
+      InputStream is = cl.getResourceAsStream(path);
+      if (is != null)
+      {
+         try
+         {
+            try
+            {
+               properties.load(is);
+            }
+            finally
+            {
+               is.close();
+            }
+         }
+         catch (IOException e)
+         {
+            throw new IllegalStateException(e);
+         }
+      }
+      return properties;
+   }
+
+   private Environment(Map<String, String> envs, Properties properties)
+   {
+      this.envs = envs;
+      this.properties = properties;
+   }
+
+   public Map<String, String> newEnvs()
+   {
+      final Map<String, String> envs = new LinkedHashMap<String, String>(this.envs);
+
+      final String javaagent = properties.getProperty("javaagent");
+      if (javaagent != null)
+      {
+         String mvnOpts = envs.get("MAVEN_OPTS");
+         if (mvnOpts == null)
+         {
+            mvnOpts = javaagent;
+         }
+         else
+         {
+            mvnOpts = (mvnOpts + " " + javaagent).trim();
+         }
+         envs.put("MAVEN_OPTS", mvnOpts);
+      }
+
+      return envs;
+   }
+
+   public Properties newProperties()
+   {
+      final Properties props = new Properties();
+      props.putAll(properties);
+      return props;
+   }
+
+   public String getProperty(String name)
+   {
+      return properties.getProperty(name);
+   }
+
+   public String getProperty(String name, String defaultValue)
+   {
+      return properties.getProperty(name, defaultValue);
+   }
+
+   public String getProperty(String name, boolean required)
+   {
+      final String value = properties.getProperty(name);
+      if (required && value == null)
+      {
+         throw new IllegalStateException("Property " + name + " is required but not set.");
+      }
+      return value;
+   }
+
+   public File getPropertyAsFile(String name)
+   {
+      final String path = getProperty(name, false);
+      if (path == null)
+      {
+         return null;
+      }
+      return new File(path);
+   }
+
+   public File getPropertyAsFile(String name, boolean required)
+   {
+      return new File(getProperty(name, required));
+   }
+
+   public File getUserHome()
+   {
+      return getPropertyAsFile("user.home", true);
+   }
+
+   public File getBuildDir()
+   {
+      return getPropertyAsFile("build.dir", true);
+   }
+
+   public File getMavenHome()
+   {
+      String mvnHome = getProperty("maven.home");
+      if (mvnHome != null)
+      {
+         return new File(mvnHome);
+      }
+
+      mvnHome = getEnv("M3_HOME", "M2_HOME", "MVN_HOME", "MAVEN_HOME");
+      if (mvnHome != null)
+      {
+         return new File(mvnHome);
+      }
+
+      final String paths = envs.get("PATH");
+      if (paths != null)
+      {
+         final File mavenHome = getMavenHome(paths);
+         if (mavenHome != null)
+         {
+            return mavenHome;
+         }
+      }
+
+      return null;
+   }
+
+   private String getEnv(String... names)
+   {
+      for (String name : names)
+      {
+         String env = envs.get(name);
+         if (env != null)
+         {
+            return env;
+         }
+      }
+      return null;
+   }
+
+   private File getMavenHome(String paths)
+   {
+      final File m2ConfFile = findFileInPaths(paths, "m2.conf");
+      return m2ConfFile == null ? null : m2ConfFile.getParentFile().getParentFile();
+   }
+
+   private File findFileInPaths(String paths, final String name)
+   {
+      for (String path : paths.split(File.pathSeparator))
+      {
+         final File file = new File(path, name);
+         if (file.exists())
+         {
+            return file;
+         }
+      }
+      return null;
+   }
+
+   public boolean isDebugAllowed()
+   {
+      return Boolean.TRUE.toString().equals(getProperty("debug.allowed"));
+   }
+
+   public File getJavaHome()
+   {
+      return getPropertyAsFile("java.home", false);
+   }
+}
---
+++ src/test/java/org/sourcepit/common/testing/EnvironmentTest.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2012 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.sourcepit.common.testing;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNull;
+import org.hamcrest.core.IsSame;
+import org.junit.Test;
+
+/**
+ * @author Bernd Vogt <bernd.vogt@sourcepit.org>
+ */
+public class EnvironmentTest
+{
+
+   @Test
+   public void testSystemProperties()
+   {
+      Environment env = Environment.newEnvironment(System.getenv(), System.getProperties());
+      assertThat(env, IsNull.notNullValue());
+
+      File userHome = env.getUserHome(); // prop
+      assertThat(userHome, IsEqual.equalTo(new File(System.getProperty("user.home"))));
+
+      File javaHome = env.getJavaHome(); // prop
+      assertThat(javaHome, IsEqual.equalTo(new File(System.getProperty("java.home"))));
+   }
+
+   @Test
+   public void testMavenHome()
+   {
+      Map<String, String> envs = new HashMap<String, String>();
+      Properties properties = new Properties();
+      Environment env = Environment.newEnvironment(envs, properties);
+
+      // null
+      assertThat(env.getMavenHome(), IsNull.nullValue());
+
+
+      // PATH
+      File file = new File("src/test/resources/maven.home/bin/");
+      assertTrue(file.exists());
+
+      envs.put("PATH", "foo" + File.pathSeparator + file.getPath() + File.pathSeparator + "murks/bin");
+      assertThat(env.getMavenHome(), IsEqual.equalTo(file.getParentFile()));
+
+
+      // ENV
+      envs.put("MAVEN_HOME", "maven-home-1");
+      assertThat(env.getMavenHome(), IsEqual.equalTo(new File("maven-home-1")));
+
+      envs.put("MVN_HOME", "maven-home-2");
+      assertThat(env.getMavenHome(), IsEqual.equalTo(new File("maven-home-2")));
+
+      envs.put("M2_HOME", "maven-home-3");
+      assertThat(env.getMavenHome(), IsEqual.equalTo(new File("maven-home-3")));
+
+      envs.put("M3_HOME", "maven-home-4");
+      assertThat(env.getMavenHome(), IsEqual.equalTo(new File("maven-home-4")));
+
+
+      // props
+      properties.setProperty("maven.home", "foo");
+      assertThat(env.getMavenHome(), IsEqual.equalTo(new File("foo")));
+   }
+
+   @Test
+   public void testDefault()
+   {
+      Environment systemEnv = Environment.getSystem();
+      Environment defaultEnv = Environment.get(null);
+      assertThat(systemEnv, IsSame.sameInstance(defaultEnv));
+
+      Map<String, String> envs = systemEnv.newEnvs();
+      for (Entry<String, String> entry : System.getenv().entrySet())
+      {
+         if ("MAVEN_OPTS".equals(entry.getKey()) && System.getProperty("javaagent") != null)
+         {
+            assertThat(entry.getValue() + " " + System.getProperty("javaagent"),
+               IsEqual.equalTo(envs.get(entry.getKey())));
+         }
+         else
+         {
+            assertThat(entry.getValue(), IsEqual.equalTo(envs.get(entry.getKey())));
+         }
+      }
+
+      Map<Object, Object> props = systemEnv.newProperties();
+      for (Entry<Object, Object> entry : System.getProperties().entrySet())
+      {
+         assertThat(entry.getValue(), IsEqual.equalTo(props.get(entry.getKey())));
+      }
+   }
+
+   @Test
+   public void testEnvProperty()
+   {
+      Map<String, String> envs = new HashMap<String, String>();
+      envs.put("FOO", "bar");
+      envs.put("USER", "yoda");
+
+      Properties props = new Properties();
+      props.put("foo", "bar");
+      props.put("env.MURKS", "pfusch");
+      props.put("env.USER", "luke");
+
+      Environment env = Environment.newEnvironment(envs, props);
+
+      assertThat(env.newEnvs().size(), Is.is(3));
+      assertThat(env.newEnvs().get("FOO"), IsEqual.equalTo("bar"));
+      assertThat(env.newEnvs().get("MURKS"), IsEqual.equalTo("pfusch"));
+      assertThat(env.newEnvs().get("USER"), IsEqual.equalTo("luke"));
+
+      assertThat(3, Is.is(env.newProperties().size()));
+      assertThat(env.newProperties().getProperty("foo"), IsEqual.equalTo("bar"));
+      assertThat(env.newProperties().getProperty("env.MURKS"), IsEqual.equalTo("pfusch"));
+      assertThat(env.newProperties().getProperty("env.USER"), IsEqual.equalTo("luke"));
+   }
+
+}
